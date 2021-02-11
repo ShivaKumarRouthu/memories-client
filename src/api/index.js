1d59b118e19cb4aa4702f5fd342a9606f799867e
Shiva Kumar Routhu <shivakumarrouthu22@gmail.com>
2021-02-11 00:41:21 +0000

    updating environemtn variables

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = process.env.MEMORIES_SERVICE? process.env.MEMORIES_Service : 'http://localhost:5000/posts';
+const url = process.env.MEMORIES_SERVICE? process.env.MEMORIES_SERVICE : 'https://memories-app-1221.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const creaetPosts = (newPost) => axios.post(url, newPost);
